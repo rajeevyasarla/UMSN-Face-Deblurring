0624cc00b5a8c4e1da058377329f5ccafaf6605a
rajeevyasarla <rajeevyasarla19@gmail.com>
2019-08-03 10:03:52 -0400

    UMSN Files

--- test_data_generation.m
+++ test_data_generation.m
@@ -16,14 +16,14 @@ for i = 3:m
         for k = 13:2:27
             blrname = strcat('./Test_data_Helen/final_Helen_blur/',im_name,'_ker',num2str(j,'%02d'),'_blur_k',num2str(k),'.png');
             B = imread(blrname);
-            blrname = strcat('./final_Helen_result/',im_name,'_ker',num2str(j,'%02d'),'_blur_k',num2str(k),'_random.png');
-            S = imread(blrname);
+            %blrname = strcat('./final_Helen_result/',im_name,'_ker',num2str(j,'%02d'),'_blur_k',num2str(k),'_random.png');
+            %S = imread(blrname);
             filename = strcat('./Testh_gt/',num2str((count),'%06d'),'.png');
             imwrite(I,filename);
             filename = strcat('./Testh/',num2str((count),'%06d'),'.png');
             imwrite(B,filename);
-            filename = strcat('./Testh_st/',num2str((count),'%06d'),'.png');
-            imwrite(S,filename);
+            %filename = strcat('./Testh_st/',num2str((count),'%06d'),'.png');
+            %imwrite(S,filename);
             count = count+1;
         end
     end
